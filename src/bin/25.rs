a8280cc5586d61290b49436e7d14115bca2cd068
Andrew Soden <andrew.soden@aviture.us.com>
2024-01-20 22:22:15 -0600

    Modify day 25 to use pointers to strings instead of cloning raw strings everywhere.

--- src/bin/25.rs
+++ src/bin/25.rs
@@ -1,12 +1,13 @@
 use itertools::Itertools;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::hash::Hash;
+use std::rc::Rc;
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq)]
 struct Edge {
-    name: String,
-    start: String,
-    end: String,
+    name: Rc<str>,
+    start: Rc<str>,
+    end: Rc<str>,
 }
 
 impl Edge {
@@ -18,29 +19,28 @@ impl Edge {
         }
     }
 }
-fn construct_graph(input: &str) -> HashMap<String, Vec<Edge>> {
-    let mut network: HashMap<String, Vec<Edge>> = HashMap::new();
+fn construct_graph(input: &str) -> HashMap<Rc<str>, Vec<Edge>> {
+    let mut network: HashMap<Rc<str>, Vec<Edge>> = HashMap::new();
 
     input.lines().for_each(|line| {
         let (key, values) = line.split_once(": ").expect("Colon seperated line");
         let connections = values
             .split_whitespace()
-            .map(|name| name.to_string())
             .collect_vec();
 
         // map edge connections of key to connections
         // connections are bidirectional and not duplicated in input
 
-        for connection in connections.iter() {
+        for connection in connections.into_iter() {
             let name = format!("{key}-{connection}");
             let edge = Edge {
-                start: key.to_string(),
-                end: connection.clone(),
-                name: name.clone(),
+                start: key.into(),
+                end: connection.into(),
+                name: name.into(),
             };
-            let node = network.entry(key.to_string()).or_default();
+            let node = network.entry(key.into()).or_default();
             node.push(edge.clone());
-            let node = network.entry(connection.clone()).or_default();
+            let node = network.entry(connection.into()).or_default();
             node.push(edge.reverse());
         }
     });
@@ -48,25 +48,25 @@ fn construct_graph(input: &str) -> HashMap<String, Vec<Edge>> {
     return network;
 }
 
-fn get_edge_to_remove(network: &HashMap<String, Vec<Edge>>) -> Edge {
-    let mut betweeness: HashMap<&String, (usize, Edge)> = HashMap::new();
+fn get_edge_to_remove(network: &HashMap<Rc<str>, Vec<Edge>>) -> Edge {
+    let mut betweeness: HashMap<Rc<str>, (usize, Edge)> = HashMap::new();
 
     for node in network.keys() {
-        let mut visited: HashSet<&String> = HashSet::new();
-        let mut queue: VecDeque<(&String, Vec<&String>)> = VecDeque::from(vec![(node, vec![])]);
+        let mut visited: HashSet<Rc<str>> = HashSet::new();
+        let mut queue: VecDeque<(Rc<str>, Vec<Rc<str>>)> = VecDeque::from(vec![(node.clone(), vec![])]);
         while let Some((node, path)) = queue.pop_front() {
-            visited.insert(node);
-            if let Some(edges) = network.get(node) {
+            visited.insert(node.clone());
+            if let Some(edges) = network.get(&node) {
                 for edge in edges.iter().filter(|edge| !visited.contains(&edge.end)) {
                     let mut next_path = path.clone();
-                    next_path.push(&edge.name);
+                    next_path.push(edge.name.clone());
                     for sub_path in next_path.iter() {
                         betweeness
-                            .entry(sub_path)
+                            .entry(sub_path.clone())
                             .and_modify(|(x, _)| *x += 1)
                             .or_insert((1, edge.clone()));
                     }
-                    queue.push_back((&edge.end, next_path));
+                    queue.push_back((edge.end.clone(), next_path));
                 }
             }
         }
@@ -80,28 +80,28 @@ fn get_edge_to_remove(network: &HashMap<String, Vec<Edge>>) -> Edge {
         .clone()
 }
 
-fn count_subgraphs(network: &HashMap<String, Vec<Edge>>) -> (usize, usize) {
+fn count_subgraphs(network: &HashMap<Rc<str>, Vec<Edge>>) -> (usize, usize) {
     //bfs ftw
     let mut visited = HashSet::new();
     // grab some node to start
-    let start = network.keys().next().unwrap();
+    let start = network.keys().next().unwrap().clone();
     let mut queue = VecDeque::from([start]);
 
     while let Some(node) = queue.pop_front() {
-        visited.insert(node);
+        visited.insert(node.clone());
 
-        if let Some(edges) = network.get(node) {
-            queue.extend(
-                edges
+        if let Some(edges) = network.get(&node) {
+            queue.append(
+                &mut edges
                     .iter()
                     .filter_map(|edge| {
                         if visited.contains(&edge.end) {
                             None
                         } else {
-                            Some(&edge.end)
+                            Some(edge.end.clone())
                         }
                     })
-                    .collect::<Vec<&String>>(),
+                    .collect::<VecDeque<_>>(),
             )
         }
     }
@@ -116,10 +116,10 @@ pub fn part_one(input: &str) -> Option<usize> {
     for _ in 0..3 {
         let removed_edge = get_edge_to_remove(&graph);
 
-        if let Some(edges) = graph.get_mut(removed_edge.start.as_str()) {
+        if let Some(edges) = graph.get_mut(&removed_edge.start) {
             edges.retain(|edge| edge.name != removed_edge.name);
         }
-        if let Some(edges) = graph.get_mut(removed_edge.end.as_str()) {
+        if let Some(edges) = graph.get_mut(&removed_edge.end) {
             edges.retain(|edge| edge.name != removed_edge.name);
         }
     }
